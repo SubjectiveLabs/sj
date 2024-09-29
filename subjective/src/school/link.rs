e5d6e3c9e5ed2b10f744daf777333d4e6be0c79e
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-09-29 23:48:32 +1000

    Change ID types and add Diff implementations

--- subjective/src/school/link.rs
+++ subjective/src/school/link.rs
@@ -1,16 +1,12 @@
 use diff::Diff;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 /// Link to websites related to a [`super::School`].
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "diff", derive(Diff))]
-#[diff(attr(
-    #[derive(Debug)]
-    #[allow(missing_docs)]
-))]
 pub struct Link {
     /// UUID of the link.
-    pub id: String,
+    pub id: Uuid,
     #[serde(rename = "title")]
     /// Name of the link.
     pub name: String,
@@ -18,4 +14,55 @@ pub struct Link {
     pub icon: String,
     /// URL that the link points to.
     pub destination: String,
+}
+
+#[derive(Debug)]
+#[cfg(feature = "diff")]
+#[allow(clippy::module_name_repetitions)]
+/// Differences between two [`Link`]s.
+pub struct LinkDiff {
+    /// Differences in the UUIDs of the links.
+    pub id: Option<Uuid>,
+    /// Differences in the names of the links.
+    pub name: Option<String>,
+    /// Differences in the icons of the links.
+    pub icon: Option<String>,
+    /// Differences in the destinations of the links.
+    pub destination: Option<String>,
+}
+
+#[cfg(feature = "diff")]
+impl Diff for Link {
+    type Repr = LinkDiff;
+
+    fn diff(&self, other: &Self) -> Self::Repr {
+        Self::Repr {
+            id: if self.id == other.id {
+                None
+            } else {
+                Some(other.id)
+            },
+            name: self.name.diff(&other.name),
+            icon: self.icon.diff(&other.icon),
+            destination: self.destination.diff(&other.destination),
+        }
+    }
+
+    fn apply(&mut self, diff: &Self::Repr) {
+        if let Some(id) = diff.id {
+            self.id = id;
+        }
+        self.name.apply(&diff.name);
+        self.icon.apply(&diff.icon);
+        self.destination.apply(&diff.destination);
+    }
+
+    fn identity() -> Self {
+        Self {
+            id: Uuid::nil(),
+            name: String::new(),
+            icon: String::new(),
+            destination: String::new(),
+        }
+    }
 }
--- subjective/src/school/notice.rs
+++ subjective/src/school/notice.rs
@@ -1,20 +1,67 @@
 use diff::Diff;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "diff", derive(Diff))]
-#[diff(attr(
-    #[derive(Debug)]
-    #[allow(missing_docs)]
-))]
 /// Notices related to a [`super::School`].
 pub struct Notice {
     /// UUID of the notice.
-    pub id: String,
+    pub id: Uuid,
     /// Title of the notice.
     pub title: String,
     /// Content of the notice.
     pub content: String,
     /// Whether the notice is a priority. If true, displayed more prominently.
     pub priority: bool,
+}
+
+#[derive(Debug)]
+#[cfg(feature = "diff")]
+#[allow(clippy::module_name_repetitions)]
+/// Differences between two [`Notice`]s.
+pub struct NoticeDiff {
+    /// Differences in the UUIDs of the notices.
+    pub id: Option<Uuid>,
+    /// Differences in the titles of the notices.
+    pub title: Option<String>,
+    /// Differences in the content of the notices.
+    pub content: Option<String>,
+    /// Differences in the priorities of the notices.
+    pub priority: Option<bool>,
+}
+
+#[cfg(feature = "diff")]
+impl Diff for Notice {
+    type Repr = NoticeDiff;
+
+    fn diff(&self, other: &Self) -> Self::Repr {
+        Self::Repr {
+            id: if self.id == other.id {
+                None
+            } else {
+                Some(other.id)
+            },
+            title: self.title.diff(&other.title),
+            content: self.content.diff(&other.content),
+            priority: self.priority.diff(&other.priority),
+        }
+    }
+
+    fn apply(&mut self, diff: &Self::Repr) {
+        if let Some(id) = diff.id {
+            self.id = id;
+        }
+        self.title.apply(&diff.title);
+        self.content.apply(&diff.content);
+        self.priority.apply(&diff.priority);
+    }
+
+    fn identity() -> Self {
+        Self {
+            id: Uuid::nil(),
+            title: String::new(),
+            content: String::new(),
+            priority: false,
+        }
+    }
 }
